80119841fbc3ba0cd083855f6025a399e21fb03e
MayuriAchewad <mayuriachewad@gmail.com>
2023-09-02 18:35:29 -0700

    added Textfield

--- src/Components/Forms/RegisterForm.jsx
+++ src/Components/Forms/RegisterForm.jsx
@@ -1,1 +1,23 @@
+import { Grid, TextField } from "@mui/material";
 import React from "react";
+
+export const RegisterForm=()=>{
+    return(
+        <form>
+            <Grid container spacing={2}>
+                <Grid item xl={12} ml={12} lg={12} mg={4} xs={4}>
+                    <TextField fullWidth variant="standard"/>
+                </Grid>
+                <Grid item xl={12} sm={12} lg={12} mg={4} xs={4}>
+                    <TextField fullWidth variant="standard"/>
+                </Grid>
+                <Grid item xl={12} ml={12} lg={12} mg={4} xs={4}>
+                    <TextField fullWidth variant="standard"/>
+                </Grid>
+                <Grid item xl={12} sm={12} lg={12} mg={4} xs={4}>
+                    <TextField fullWidth variant="standard"/>
+                </Grid>
+            </Grid>
+        </form>
+    )
+}
